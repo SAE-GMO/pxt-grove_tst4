f7817c67e689b700b08c72751db2c80ec317d394
schaedejo00 <j.schaede@gymnasium-oberstadt.de>
2023-04-26 15:26:34 +0200

    added default url

--- main.ts
+++ main.ts
@@ -746,6 +746,7 @@ namespace grove {
     //% block="Send Data to your ThinkSpeak Channel|URL %url|Write API Key %apiKey|Field1 %field1|Field2 %field2|Field3 %field3|Field4 %field4|Field5 %field5|Field6 %field6|Field7 %field7|Field8 %field8"
     //% group="UartWiFi"
     //% apiKey.defl="your Write API Key"
+    //% url.defl="thingspeak.com"
     export function sendToThinkSpeak(url: string = "thingspeak.com", apiKey: string, field1: number, field2: number, field3: number, field4: number, field5: number, field6: number, field7: number, field8: number) {
         let result = 0
         let retry = 2
